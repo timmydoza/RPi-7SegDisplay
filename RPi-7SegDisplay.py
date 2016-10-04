ab46b8eafcd3152b1a2ac4502f7612043d3f2fbd
Tim Mendoza <timmydoza@gmail.com>
2016-10-04 00:58:53 -0400

    refactor

--- RPi-7SegDisplay.py
+++ RPi-7SegDisplay.py
@@ -1,5 +1,4 @@
 import RPi.GPIO as GPIO
-from math import pow
 import multiprocessing
 from time import sleep
 
@@ -15,7 +14,8 @@ _CHARACTERS = {
     '8': 0b10000000,
     '9': 0b10010000,
     ' ': 0b11111111,
-    '-': 0b10111111
+    '-': 0b10111111,
+    '.': 0b01111111
     }
 
 _BLANK = [_CHARACTERS[' ']] * 8
@@ -29,9 +29,7 @@ def init(data_pin, clock_pin, latch_pin):
 
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(_data, GPIO.OUT)
-    GPIO.setup(_clock, GPIO.OUT)
-    GPIO.setup(_latch, GPIO.OUT)
+    GPIO.setup([data_pin, clock_pin, latch_pin], GPIO.OUT)
 
     if (len(multiprocessing.active_children()) == 0):
         _proc = multiprocessing.Process(target=_display)
@@ -39,21 +37,19 @@ def init(data_pin, clock_pin, latch_pin):
 
 
 def _shift(byte):
-    for i in range(8)[::-1]:
-        if (byte & (1 << i)):
-            GPIO.output(_data, 1)
-        else:
-            GPIO.output(_data, 0)
+    for i in range(7, -1, -1):
+        GPIO.output(_data, byte & (1 << i))
         GPIO.output(_clock, 1)
         GPIO.output(_clock, 0)
 
 
 def _write(message):
-    for i in range(len(message)):
-        _shift(int(pow(2, i)))
-        _shift(message[i])
+    for i, char in enumerate(message):
+        _shift(2 << i)
+        _shift(char)
         GPIO.output(_latch, 1)
         GPIO.output(_latch, 0)
+        sleep(0.002)
 
 
 def _display():
@@ -61,17 +57,16 @@ def _display():
         if not _queue.empty():
             text = _queue.get()
         _write(text)
-        sleep(0.002)
 
 
 def show(message):
     result = []
     if not _queue.full():
-        for i in range(len(message)):
-            if (message[i] == '.' and i > 0):
-                result[-1] = result[-1] - 128
-            elif (message[i] != '.'):
-                result.append(_CHARACTERS[message[i]])
+        for i, char in enumerate(message):
+            if (char == '.' and i > 0):
+                result[-1] -= 128
+            elif (char != '.'):
+                result.append(_CHARACTERS[char])
         _queue.put(result)
 
 
